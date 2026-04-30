8c53e4307f50df8df8f3875191afe3d788f45cc7
Mufasa <muhiaanderson4@gmail.com>
2026-04-30 13:19:27 +0300

    Install and configure Jazzmin for a beautiful admin UI

--- profit_tracker_pro/settings.py
+++ profit_tracker_pro/settings.py
@@ -8,6 +8,7 @@ ALLOWED_HOSTS = ['*']
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -17,6 +18,56 @@ INSTALLED_APPS = [
     'django_bootstrap5',
     'tracker',
 ]
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Profit Tracker Admin",
+    "site_header": "Profit Tracker",
+    "site_brand": "Mufasa's Inventory",
+    "welcome_sign": "Welcome back, Mufasa!",
+    "search_model": ["tracker.Product"],
+    "show_sidebar": True,
+    "navigation_expanded": True,
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "tracker.Product": "fas fa-box",
+        "tracker.Transaction": "fas fa-exchange-alt",
+    },
+    "order_with_respect_to": ["tracker", "auth"],
+}
+
+JAZZMIN_UI_CONFIG = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-dark",
+    "accent": "accent-primary",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "flatly",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
